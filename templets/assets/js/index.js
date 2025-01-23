86dc0cdfdd10dbc903ae3576d30e30a176991e2d
Ermias80 <ermiasmaz8069@gmail.com>
2025-01-23 23:46:54 +0300

    index javascript

---
+++ templets/assets/js/index.js
@@ -0,0 +1,7 @@
+function redirectToSignIn() {
+    window.location.href = 'sign_in.html'; // URL for the login page
+}
+
+function redirectToSignUp() {
+    window.location.href = 'sign_up.html'; // URL for the registration page
+}
